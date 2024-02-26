b761f892a8eefa732e110eb5c74cc1ad949a08ae
2realzoo <lunalee00@gmail.com>
2024-02-26 18:50:48 +0900

    Tweeting to Firestore

--- src/firebase.tsx
+++ src/firebase.tsx
@@ -1,5 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBjqsW6DA4iFHh2oWiQ89lFzB6joR-HCa8",
@@ -7,9 +9,13 @@ const firebaseConfig = {
   projectId: "nwitter-reloaded-1d18a",
   storageBucket: "nwitter-reloaded-1d18a.appspot.com",
   messagingSenderId: "897816913403",
-  appId: "1:897816913403:web:500b590600be384c70575e"
+  appId: "1:897816913403:web:500b590600be384c70575e",
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
+
+export const storage = getStorage(app);
+
+export const db = getFirestore(app);
